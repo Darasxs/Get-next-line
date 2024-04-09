ae90f39006bf765313f36797aaa933169243802b
Dariusz Paluszkiewicz <dpaluszk@2-E-8.42heilbronn.de>
2024-04-09 22:53:43 +0200

    trying to one function

--- get_next_line.c
+++ get_next_line.c
@@ -6,36 +6,39 @@
 /*   By: dpaluszk <dpaluszk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/07 12:51:28 by dpaluszk          #+#    #+#             */
-/*   Updated: 2024/04/09 18:59:35 by dpaluszk         ###   ########.fr       */
+/*   Updated: 2024/04/09 22:53:21 by dpaluszk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+char	*free_helper(char *s1, char *s2)
+{
+	free(s1);
+	free(s2);
+	return (NULL);
+}
 
 char	*read_new_line(int fd, char *my_buffer)
 {
 	ssize_t	reading_bytes;
 	char	*new_line;
 
-	new_line = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	reading_bytes = 1;
+	new_line = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!new_line)
 		return (NULL);
 	new_line[BUFFER_SIZE] = '\0';
-	reading_bytes = 1;
-	while (reading_bytes > 0 && ft_memchr(new_line, '\n',
-			reading_bytes) == NULL)
+	while (reading_bytes > 0 && (ft_memchr(new_line, '\n',
+				reading_bytes) == NULL))
 	{
 		reading_bytes = read(fd, new_line, BUFFER_SIZE);
 		if (reading_bytes == -1)
-		{
-			free (my_buffer);
-			free (new_line);
-			return (NULL);
-		}
+			return (free_helper(my_buffer, new_line));
 		new_line[reading_bytes] = '\0';
 		my_buffer = ft_strjoin(my_buffer, new_line);
 	}
-	free (new_line);
+	free(new_line);
 	return (my_buffer);
 }
 
@@ -44,7 +47,25 @@ char	*get_next_line(int fd)
 	static char	*my_buffer;
 	char		*tmp;
 
+	my_buffer = (char *)malloc(1 * sizeof(char));
+	my_buffer[0] = 0;
 	my_buffer = read_new_line(fd, my_buffer);
 	if (!my_buffer)
 		return (NULL);
+	return (my_buffer);
+}
+int	main(void)
+{
+	int fd;
+	char *line;
+	fd = open("test.txt", O_RDONLY);
+	if (fd == -1)
+	{
+		perror("Error opening file");
+		return (1);
+	}
+	line = get_next_line(fd);
+	printf("%s", line);
+	close(fd);
+	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,19 +6,27 @@
 /*   By: dpaluszk <dpaluszk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/07 12:51:31 by dpaluszk          #+#    #+#             */
-/*   Updated: 2024/04/09 18:34:11 by dpaluszk         ###   ########.fr       */
+/*   Updated: 2024/04/09 21:22:41 by dpaluszk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE.H
-# define GET_NEXT_LINE .H
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
 # define BUFFER_SIZE 42
+# endif
 
 # include <unistd.h>
 # include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
 
 char	*get_next_line(int fd);
+char	*read_new_line(int fd, char *my_buffer);
+char	*free_helper(char *s1, char *s2);
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(const char *s1, const char *s2);
+void	*ft_memchr(const void *s, int c, size_t n);
 
 #endif
