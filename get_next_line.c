c379e7de5a279f35429a1effed14c04e42b51416
Dariusz Paluszkiewicz <dpaluszk@2-E-4.42heilbronn.de>
2024-04-09 19:02:28 +0200

    first helper function done

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: dpaluszk <dpaluszk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/07 12:51:28 by dpaluszk          #+#    #+#             */
-/*   Updated: 2024/04/09 13:34:39 by dpaluszk         ###   ########.fr       */
+/*   Updated: 2024/04/09 18:59:35 by dpaluszk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,26 @@ char	*read_new_line(int fd, char *my_buffer)
 	ssize_t	reading_bytes;
 	char	*new_line;
 
-	reading_bytes = read(fd, my_buffer, BUFFER_SIZE);
-	if (reading_bytes == -1)
+	new_line = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (!new_line)
 		return (NULL);
-	while (reading_bytes > 0 && my_buffer[reading_bytes - 1] != '\n')
+	new_line[BUFFER_SIZE] = '\0';
+	reading_bytes = 1;
+	while (reading_bytes > 0 && ft_memchr(new_line, '\n',
+			reading_bytes) == NULL)
 	{
+		reading_bytes = read(fd, new_line, BUFFER_SIZE);
 		if (reading_bytes == -1)
+		{
+			free (my_buffer);
+			free (new_line);
 			return (NULL);
-		new_line = ft_calloc(sizeof(char) * reading_bytes + 1);
-		if (!new_line)
-			return (NULL);
-		reading_bytes = read(fd, my_buffer, BUFFER_SIZE);
-		if (reading_bytes == -1)
-			return (NULL);
-
+		}
+		new_line[reading_bytes] = '\0';
+		my_buffer = ft_strjoin(my_buffer, new_line);
 	}
+	free (new_line);
+	return (my_buffer);
 }
 
 char	*get_next_line(int fd)
@@ -42,5 +47,4 @@ char	*get_next_line(int fd)
 	my_buffer = read_new_line(fd, my_buffer);
 	if (!my_buffer)
 		return (NULL);
-	tmp = my_buffer;
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,32 +6,56 @@
 /*   By: dpaluszk <dpaluszk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/07 12:51:17 by dpaluszk          #+#    #+#             */
-/*   Updated: 2024/04/08 16:00:13 by dpaluszk         ###   ########.fr       */
+/*   Updated: 2024/04/09 18:52:45 by dpaluszk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-void	ft_bzero(void *s, size_t n)
+size_t	ft_strlen(const char *s)
 {
-	unsigned char	*ptr;
-	size_t			i;
+	size_t	i;
 
-	ptr = (unsigned char *)s;
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	char	*new;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	new = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (!new)
+		return (NULL);
+	while (s1[i] != '\0')
+		new[j++] = s1[i++];
+	i = 0;
+	while (s2[i] != '\0')
+		new[j++] = s2[i++];
+	new[j] = '\0';
+	return (new);
+}
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t			i;
+	unsigned char	*str;
+
+	str = (unsigned char *)s;
 	i = 0;
 	while (i < n)
 	{
-		ptr[i] = 0;
+		if (str[i] == (unsigned char)c)
+			return ((void *)&str[i]);
 		i++;
 	}
-}
-void	*calloc(size_t count, size_t size)
-{
-	void *ptr;
-
-	ptr = (void *)malloc(count * size);
-	if (ptr == NULL)
-		return (NULL);
-	ft_bzero(ptr, count * size);
-	return (ptr);
+	return (NULL);
 }
